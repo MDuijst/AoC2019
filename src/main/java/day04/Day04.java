cb1e961cbc54535562eb067d4b8fef9f1d1ce9d0
M.C. Duijst <mcduijst@gmail.com>
2019-12-04 12:05:49 +0100

    AoC day 4 part 1

--- src/main/java/day04/Day04.java
+++ src/main/java/day04/Day04.java
@@ -1,4 +1,75 @@
 package day04;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Day04 {
+    public static int startingValue = 278384;
+    public static int finalValue = 824795;
+//    int[] range = new int[];
+
+//    It is a six-digit number.
+//    The value is within the range given in your puzzle input.
+//    Two adjacent digits are the same (like 22 in 122345).
+//    Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+
+    public static boolean validPassword(int[] input) {
+//        String  password = input;
+//        String[] passwordString = password.split("",1);
+//        System.out.println("Current array: " + passwordString[0]+", " +passwordString[1]+", " +passwordString[2]+", " +passwordString[3]+", " +passwordString[4]+", " +passwordString[5]);
+        //int[] passwordValues = new int[];
+
+        //for(int i=0; i<password.length(); i++) {
+        //    passwordValues[i] = password.split();
+        //}
+
+        int[] passwordValues = input;
+        boolean isValid = false;
+        if(passwordValues[0]==passwordValues[1] ||
+                passwordValues[1]==passwordValues[2] ||
+                passwordValues[2]==passwordValues[3] ||
+                passwordValues[3]==passwordValues[4] ||
+                passwordValues[4]==passwordValues[5] ) {
+            if(passwordValues[0]<=passwordValues[1] &&
+                    passwordValues[1]<=passwordValues[2] &&
+                    passwordValues[2]<=passwordValues[3] &&
+                    passwordValues[3]<=passwordValues[4] &&
+                    passwordValues[4]<=passwordValues[5]) {
+                isValid = true;
+            }
+        }
+        return isValid;
+    }
+
+    public static ArrayList<Integer> getListOfValidPaswords() {
+        ArrayList<Integer> range = new ArrayList<Integer>();
+        for (int password = startingValue; password <= finalValue; password++) {
+            String passwordAsString = String.valueOf(password);
+            char[] passwordAsChar = passwordAsString.toCharArray();
+            int[] passwordAsInt = new int[6];
+            passwordAsInt[0] = Integer.valueOf(Character.toString(passwordAsChar[0]));
+            passwordAsInt[1] = Integer.valueOf(Character.toString(passwordAsChar[1]));
+            passwordAsInt[2] = Integer.valueOf(Character.toString(passwordAsChar[2]));
+            passwordAsInt[3] = Integer.valueOf(Character.toString(passwordAsChar[3]));
+            passwordAsInt[4] = Integer.valueOf(Character.toString(passwordAsChar[4]));
+            passwordAsInt[5] = Integer.valueOf(Character.toString(passwordAsChar[5]));
+
+            boolean isPasswordValid = validPassword(passwordAsInt);
+            if (isPasswordValid) {
+                range.add(password);
+            }
+        }
+        return range;
+    }
+
+    public static int countNumberOfValidPasswords(ArrayList<Integer> input) {
+        ArrayList<Integer> validPasswords = input;
+        return validPasswords.size();
+    }
+
+    public static int puzzleA () {
+        return countNumberOfValidPasswords(getListOfValidPaswords());
+    }
+
 }
--- src/test/java/day04/Day04Test.java
+++ src/test/java/day04/Day04Test.java
@@ -2,18 +2,75 @@ package day04;
 
 import org.junit.jupiter.api.Test;
 
+import static day04.Day04.puzzleA;
+import static day04.Day04.validPassword;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Day04Test {
 
     @Test
     void testExampleOne() {
-        int input = 12;
+        String password = "122345";
+        int[] input = new int[] {1,2,2,3,4,5};
+//        System.out.println("password "+ password + " is valid is " + validPassword(input));
+        assertTrue(validPassword(input));
 //        assertEquals(2,//);
+    }
+    @Test
+    void testExampleTwo() {
+        String password = "111123";
+        int[] input = new int[] {1,1,1,1,2,3};
+//        System.out.println("password "+ password + " is valid is " + validPassword(input));
+        assertTrue(validPassword(input));
+//        assertEquals(2,//);
+    }
+    @Test
+    void testExampleThree() {
+        String password = "111111";
+        int[] input = new int[] {1,1,1,1,1,1};
+//        System.out.println("password "+ password + " is valid is " + validPassword(input));
+        assertTrue(validPassword(input));
+//        assertEquals(2,//);
+    }
+    @Test
+    void testExampleFour() {
+        String password = "223450";
+        int[] input = new int[] {2,2,3,4,5,0};
+//        System.out.println("password "+ password + " is valid is " + validPassword(input));
+//        assertEquals(2,//);
+        assertFalse(validPassword(input));
+    }
+    @Test
+    void testExampleFive() {
+        String password = "123789";
+        int[] input = new int[] {1,2,3,7,8,9};
+//        System.out.println("password "+ password + " is valid is " + validPassword(input));
+//        assertEquals(2,//);
+        assertFalse(validPassword(input));
+    }
+
+    @Test
+    void testExampleSix() {
+        String password = "123444";
+        int[] input = new int[] {1,2,3,4,4,4};
+//        System.out.println("password "+ password + " is valid is " + validPassword(input));
+//        assertEquals(2,//);
+        assertFalse(validPassword(input));
+    }
+    @Test
+    void testExampleSeven() {
+        String password = "111122";
+        int[] input = new int[] {1,1,1,1,2,2};
+//        System.out.println("password "+ password + " is valid is " + validPassword(input));
+//        assertEquals(2,//);
+        assertTrue(validPassword(input));
     }
 
     @Test
     void puzzle() {
-        String input="";
-        System.out.println("Day01(a): ");
+        //String input="";
+        System.out.println("Day01(a): " + puzzleA());
         System.out.println("Day01(b): ");
     }
 
